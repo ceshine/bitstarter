fe4044f1dbcd23f6f0be5260c07efccdd93390e5
CeShine Lee <shuanck@gmail.com>
2013-07-08 22:37:08 +0800

    update web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readeFileSync('index.html', function (err, data) {
+	if (err) throw err;
+	var buffer = new Buffer(data, 'utf-8');
+	response.send(buffer.toString("utf-8");
+  });
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
